a7fdae35bac49eecaa1f06fd283d010d43861555
Stef <smarchal74alice@gmail.com>
2020-10-16 22:38:22 +0200

    Add a supervillain name randomly

--- externalapi.js
+++ externalapi.js
@@ -1,5 +1,8 @@
 //third which is external api inwhich api is used
 
-const superheroes = require('superheroes');
+const superheroes = require("superheroes");
+
+const supervillains = require("supervillains");
 
 console.log(superheroes.random());
+console.log(supervillains.random());
